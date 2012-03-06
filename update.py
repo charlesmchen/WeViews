f5757f2a96e8a9ce50e44bb55c096b9756bb3486
Charles Matthew Chen <charlesmchen@gmail.com>
2012-03-06 21:10:43 -0200

    More improvements to the project pages.

--- update.py
+++ update.py
@@ -8,39 +8,40 @@ for filename in os.listdir(os.curdir):
   if filename.endswith('.html'):
     htmlFiles.append(filepath)
 
-for htmlFile in htmlFiles:
-  print htmlFile
-  with open(htmlFile, 'rt') as f:
-    text = f.read()
-
-  filename = os.path.basename(htmlFile)
-  openKey = '<!-- INSERT %s BEGIN -->' % filename
-  endKey = '<!-- INSERT %s END -->' % filename
-  print 'openKey', openKey
-  print 'endKey', endKey
+for ignore in xrange(3):
+    for htmlFile in htmlFiles:
+      print htmlFile
+      with open(htmlFile, 'rt') as f:
+        text = f.read()
     
-  for otherFile in htmlFiles:
-      if htmlFile == otherFile:
-          continue
-
-      with open(otherFile, 'rt') as f:
-        otherText = f.read()
+      filename = os.path.basename(htmlFile)
+      openKey = '<!-- INSERT %s BEGIN -->' % filename
+      endKey = '<!-- INSERT %s END -->' % filename
+      print 'openKey', openKey
+      print 'endKey', endKey
         
-      startIndex = otherText.find(openKey)
-      endIndex = otherText.find(endKey)
-#      print '\t', 'otherFile', otherFile
-#      print '\t\t', 'startIndex', startIndex
-#      print '\t\t', 'endIndex', endIndex
-      if (startIndex >= 0) and (endIndex > startIndex):
-          otherText = (otherText[:startIndex + len(openKey)] 
-+ '\n'
-+ text
-+ '\n'
-+ otherText[endIndex:])
-#          print '\t', 'otherText', otherText
-          with open(otherFile, 'wt') as f:
-            f.write(otherText)
-      
+      for otherFile in htmlFiles:
+          if htmlFile == otherFile:
+              continue
+    
+          with open(otherFile, 'rt') as f:
+            otherText = f.read()
+            
+          startIndex = otherText.find(openKey)
+          endIndex = otherText.find(endKey)
+    #      print '\t', 'otherFile', otherFile
+    #      print '\t\t', 'startIndex', startIndex
+    #      print '\t\t', 'endIndex', endIndex
+          if (startIndex >= 0) and (endIndex > startIndex):
+              otherText = (otherText[:startIndex + len(openKey)] 
+    + '\n'
+    + text
+    + '\n'
+    + otherText[endIndex:])
+    #          print '\t', 'otherText', otherText
+              with open(otherFile, 'wt') as f:
+                f.write(otherText)
+          
 
   
   
